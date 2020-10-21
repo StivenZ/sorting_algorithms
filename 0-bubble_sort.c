7931f354965c31dc11077195113cfec468190096
stivenz <1806@holbertonschool.com>
2020-10-21 08:14:15 +0000

    Fixed style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,23 +6,23 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i = 0, tmp = 0;
+	size_t i = 0, tmp = 0;
 
-    if (size < 2)
-        return;
+	if (size < 2)
+		return;
 
-    while (array[i] && i < size)
-    {
-        if (array[i] > array[i + 1] && (array[i + 1]))
-        {
-            tmp = array[i];
-            array[i] = array[i + 1];
-            array[i + 1] = tmp;
-            if (array[i - 1])
-                i--;
-            print_array(array, size);
-        }
-        else
-            i++;
-    }
+	while (array[i] && i < size)
+	{
+		if (array[i] > array[i + 1] && (array[i + 1]))
+		{
+			tmp = array[i];
+			array[i] = array[i + 1];
+			array[i + 1] = tmp;
+			if (array[i - 1])
+				i--;
+			print_array(array, size);
+		}
+		else
+			i++;
+	}
 }
