1c52f223233e59f9ef29a68794d72dc2699a1491
stivenz <1806@holbertonschool.com>
2020-12-02 22:02:17 -0500

    Selection algorithm and header

--- sort.h
+++ sort.h
@@ -22,6 +22,9 @@ int _putchar(char c);
 int prints(char *s);
 
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 
